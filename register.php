9fe4b355d5c75cf634f3e47515c825dcc98eef58
lenharshi <harshithareddy495@gmail.com>
2018-11-10 14:50:17 +0530

    first commit

---
+++ register.php
@@ -0,0 +1,15 @@
+<?php
+include "conn.php";
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+
+	$query="INSERT INTO `users`(`name`,`email`,`password`) VALUES ('$name','$email','$password')";
+	$result=mysqli_query($con,$query);
+	if($result){
+		echo"successfully Registered";
+	}
+	else{
+		echo"Error in registeration";
+	}
+	?>
